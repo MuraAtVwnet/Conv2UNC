4d0b3b89adb39355c6116d2cd963278e1033937e
Syuichi Murashima <mura@vwnet.jp>
2018-11-27 12:53:54 +0900

    SendTo でうまくファイル名が渡せなかったので、暫定版として Profile 仕込み用 function にした

--- Conv2UNC.ps1
+++ Conv2UNC.ps1
@@ -1,32 +1,27 @@
-﻿# SendTo
-# ドライブレター取出し
-# Get-Psdrive でドライブレターを　UNC に変換
+﻿function Conv2UNC( $TergetPath ){
+	$DriveLetter = Split-Path $TergetPath -Qualifier -ErrorAction SilentlyContinue
 
-Param( $TergetPath )
+	if( $DriveLetter -ne $null ){
+		$DriveName = $DriveLetter.Replace(":","")
+		$NetworkRoot = (Get-PSDrive $DriveName).DisplayRoot
+		$TergetDirectory = Split-Path  $TergetPath -NoQualifier
+		$NetworkPath = Join-Path $NetworkRoot $TergetDirectory
+	}
+	else{
+		$NetworkPath = $TergetPath
+	}
 
-# $TergetPath = "I:\04_セキュリティ横断\48_マルウェア技術ドキュメント\CM77\CLI_CR_770.pdf"
-$DriveLetter = Split-Path $TergetPath -Qualifier -ErrorAction SilentlyContinue
+	$URI = "<file://" + $NetworkPath + ">"
 
-if( $DriveLetter -ne $null ){
-	$DriveName = $DriveLetter.Replace(":","")
-	$NetworkRoot = (Get-PSDrive $DriveName).DisplayRoot
-	$TergetDirectory = Split-Path  $TergetPath -NoQualifier
-	$NetworkPath = Join-Path $NetworkRoot $TergetDirectory
+
+
+	# エンコードを S-JIS に変更
+	$OutputEncoding = [Console]::OutputEncoding
+
+	# リップボードへ
+	$URI | clip
+
+	# エンコードを US-ASCII に戻す
+	$OutputEncoding = New-Object System.Text.ASCIIEncoding
 }
-else{
-	$NetworkPath = $TergetPath
-}
-
-$URI = "<file://" + $NetworkPath + ">"
-
-# エンコードを S-JIS に変更
-$OutputEncoding = [Console]::OutputEncoding
-
-# リップボードへ
-$URI | clip
-
-# エンコードを US-ASCII に戻す
-$OutputEncoding = New-Object System.Text.ASCIIEncoding
-
-
 
