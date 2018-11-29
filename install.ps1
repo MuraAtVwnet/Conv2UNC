4e0b8fdd3b0222ae3c13aef753ba6f6e65887316
Syuichi Murashima <mura@vwnet.jp>
2018-11-29 09:53:04 +0900

    インストール先を AppData から取ることにした AllUser オプションは期待動作しなかったのでいったん保留

--- install.ps1
+++ install.ps1
@@ -1,5 +1,13 @@
-﻿#SendToフォルダパス
-$SendToDirectory = Join-Path $env:USERPROFILE "\AppData\Roaming\Microsoft\Windows\SendTo"
+﻿# Param( [switch]$AllUser )
+
+#SendToフォルダパス
+$SendToDirectory = Join-Path $env:APPDATA "\Microsoft\Windows\SendTo"
+
+# 全ユーザー対象の時は Default に入れる(期待動作しなかったのでコメントアウト)
+# if( $AllUser ){
+#	$UserName = $env:USERNAME
+#	$SendToDirectory = $SendToDirectory.Replace($UserName, "Default")
+# }
 
 # リンクフルパス
 $LinkFullPath = Join-Path $SendToDirectory "Path to URI.lnk"
