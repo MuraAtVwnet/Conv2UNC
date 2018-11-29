760ecde9a95f71e07d53c7a556cda7febbc8ca28
Syuichi Murashima <mura@vwnet.jp>
2018-11-29 09:53:54 +0900

    uninstall 追加

---
+++ uninstall.ps1
@@ -0,0 +1,17 @@
+﻿# Param( [switch]$AllUser )
+
+#SendToフォルダパス
+$SendToDirectory = Join-Path $env:APPDATA "\Microsoft\Windows\SendTo"
+
+# 全ユーザー対象の時は Default に入れる(期待動作しなかったのでコメントアウト)
+# if( $AllUser ){
+#	$UserName = $env:USERNAME
+#	$SendToDirectory = $SendToDirectory.Replace($UserName, "Default")
+# }
+
+# リンクフルパス
+$LinkFullPath = Join-Path $SendToDirectory "Path to URI.lnk"
+
+if( Test-Path $LinkFullPath ){
+	Remove-Item $LinkFullPath
+}
