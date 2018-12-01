df027fabd4b1c0949b44b3be1868b979d1b1a338
Syuichi Murashima <mura+develop@vwnet.jp>
2018-12-01 14:48:18 +0900

    ショートカットが指定されていた場合対応

--- Conv2UNC.ps1
+++ Conv2UNC.ps1
@@ -2,25 +2,48 @@
 # 物理 Path を求める
 ###########################################################
 function ConvL2P( $TergetPath ){
-	$DriveLetter = Split-Path $TergetPath -Qualifier -ErrorAction SilentlyContinue
 
-	if( $DriveLetter -ne $null ){
-		$DriveName = $DriveLetter.Replace(":","")
-		$NetworkRoot = (Get-PSDrive $DriveName).DisplayRoot
-		if( $NetworkRoot -ne $null ){
-			$TergetDirectory = Split-Path  $TergetPath -NoQualifier
-			$PhysicalPath = Join-Path $NetworkRoot $TergetDirectory
+	if( $TergetPath -ne $null ){
+		$DriveLetter = Split-Path $TergetPath -Qualifier -ErrorAction SilentlyContinue
+
+		if( $DriveLetter -ne $null ){
+			$DriveName = $DriveLetter.Replace(":","")
+			$NetworkRoot = (Get-PSDrive $DriveName).DisplayRoot
+			if( $NetworkRoot -ne $null ){
+				$TergetDirectory = Split-Path  $TergetPath -NoQualifier
+				$PhysicalPath = Join-Path $NetworkRoot $TergetDirectory
+			}
+			else{
+				$PhysicalPath = $TergetPath
+			}
 		}
 		else{
 			$PhysicalPath = $TergetPath
 		}
 	}
 	else{
-		$PhysicalPath = $TergetPath
+		$PhysicalPath = $null
 	}
 
 	return $PhysicalPath
 }
+
+###########################################################
+# ショートカットのリンク先を得る
+###########################################################
+function GetLink( $LinkFullPath ){
+	if( Test-Path $LinkFullPath ){
+		$WsShell = New-Object -ComObject WScript.Shell
+		$Shortcut = $WsShell.CreateShortcut($LinkFullPath)
+		$Link = $Shortcut.TargetPath
+	}
+	else{
+		$Link = $null
+	}
+
+	return $Link
+}
+
 
 ###########################################################
 # URI にし、クリップボートに転送
@@ -28,17 +51,21 @@ function ConvL2P( $TergetPath ){
 function SetURI2Clip($PhysicalPaths){
 	$URIs = @()
 	foreach( $PhysicalPath in $PhysicalPaths ){
-		$URIs += "<file://" + $PhysicalPath + ">"
+		if( $PhysicalPath -ne $null ){
+			$URIs += "<file://" + $PhysicalPath + ">"
+		}
 	}
 
-	# エンコードを S-JIS に変更
-	$OutputEncoding = [Console]::OutputEncoding
+	if( $URIs.Count -ne 0 ){
+		# エンコードを S-JIS に変更
+		$OutputEncoding = [Console]::OutputEncoding
 
-	# リップボードへ
-	$URIs | clip
+		# リップボードへ
+		$URIs | clip
 
-	# エンコードを US-ASCII に戻す
-	$OutputEncoding = New-Object System.Text.ASCIIEncoding
+		# エンコードを US-ASCII に戻す
+		$OutputEncoding = New-Object System.Text.ASCIIEncoding
+	}
 }
 
 
@@ -49,7 +76,15 @@ function SetURI2Clip($PhysicalPaths){
 if( $args.Count -ne 0 ){
 	$PhysicalPaths = @()
 	foreach( $arg in $args ){
+		# シートカットだったら Link にする
+		if( $arg -like "*.lnk" ){
+			$arg = GetLink $arg
+		}
+
+		# 物理 Path 取得
 		$PhysicalPaths += ConvL2P $arg
 	}
+
+	# クリップボード に送る
 	SetURI2Clip $PhysicalPaths
 }
